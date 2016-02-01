3c4b250837fe9f5687bf6d8e3807f41272ab4988
Alonso Méndez <alonsomendez@gmail.com>
2016-02-01 15:32:27 -0700

    Adding base code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,26 @@
-console.log('it works!');
+'use strict';
+
+require('jsplumb');
+require('jquery');
+
+jsPlumb.bind("ready", function() {
+    var firstInstance = jsPlumb.getInstance();
+
+    firstInstance.importDefaults({
+        Connector : [ "Bezier", { curviness: 150 } ],
+        Anchors : [ "TopCenter", "BottomCenter" ]
+    });
+
+    firstInstance.connect({
+        source:"element1",
+        target:"element2",
+        scope:"someScope"
+    });
+
+    //firstInstance.setId('rootID');
+
+    firstInstance.setContainer($("body"));
+
+    console.log('its working!', firstInstance);
+
+});
